8eab3b6fb7d2ee1066902038b414c312a40c24aa
Pedrohph <micosacpv@hotmail.com>
2020-10-31 03:21:21 -0300

    Camera following Player
    -Camera follows player movement
    -Controller base added but not finished

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    //inside class
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
+
+    public PlayerMovement player;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Swipe();
+    }
+
+    public void Swipe() {
+        if (Input.GetMouseButtonDown(0)) {
+            //save began touch 2d point
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButton(0)) {
+            //save ended touch 2d point
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            //normalize the 2d vector
+            currentSwipe.Normalize();
+
+            /*
+            //swipe upwards
+            if (currentSwipe.y > 0 ) {
+                Debug.Log("up swipe");
+            }
+            //swipe down
+            if (currentSwipe.y < 0) {
+                Debug.Log("down swipe");
+            }
+            //swipe left
+            if (currentSwipe.x < 0 ) {
+                Debug.Log("left swipe");
+            }
+            //swipe right
+            if (currentSwipe.x > 0) {
+                Debug.Log("right swipe");
+            }*/
+
+
+            Debug.Log(currentSwipe);
+            player.MovePlayer(currentSwipe.x, currentSwipe.y);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [Header("Movimentacao")]
+    public float movementSpeed;
+    public float leanLimit;
+    public float lerpTime;
+
+    public Vector2 movementLimits;
+
+    [Header("Visao")]
+    public float lookSpeed;
+    public Transform aimTarget;
+
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        PlayerMove(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        RotationLook(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        HorizontalLean(transform, Input.GetAxis("Horizontal"));
+    }
+
+    public void MovePlayer(float x, float y) {
+        PlayerMove(x, y);
+        RotationLook(x, y);
+        HorizontalLean(transform, x);
+    }
+
+    public void PlayerMove(float x, float y) {
+        transform.localPosition += new Vector3(x, y, 0) * movementSpeed * Time.deltaTime;
+        ClampPosition();
+    }
+
+    void ClampPosition() {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -movementLimits.x, movementLimits.x), Mathf.Clamp(transform.position.y , -movementLimits.y, movementLimits.y),0);// Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    public void RotationLook(float h, float v) {
+        aimTarget.parent.position = Vector3.zero;
+        aimTarget.localPosition = new Vector3(h, v, 1);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * movementSpeed);
+    }
+
+    void HorizontalLean(Transform target, float axis) {
+        Vector3 targetEulerAngels = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
+    }
+
+}
