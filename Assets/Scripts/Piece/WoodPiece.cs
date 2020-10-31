9d1e1c7236ea7d351c66f8d0e206458b4663fff3
Pedrohph <micosacpv@hotmail.com>
2020-10-31 20:58:26 -0300

    Trees and Wood Pieces

--- Assets/Scripts/Piece/WoodPiece.cs
+++ Assets/Scripts/Piece/WoodPiece.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WoodPiece : Obstacles{
+    public WoodPiece UpperWood;
+    public WoodPiece LowerWood;
 
     public void RemoveJoint() {
+        gameObject.GetComponent<Rigidbody>().AddForce(0, 0, 10, ForceMode.Impulse);
         if (GetComponent<FixedJoint>())
             GetComponent<FixedJoint>().breakForce = 0;
     }
@@ -12,6 +15,7 @@ public class WoodPiece : Obstacles{
     public override void WingCollision() {
         PlaySound(AudioCut);
         RemoveJoint();
+        LowerWood.RemoveJoint();
     }
 
     public override void BodyCollision() {
