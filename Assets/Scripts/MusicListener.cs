0cb07eaf5aadee0207befe214345dfe4eca76ed9
Pedrohph <micosacpv@hotmail.com>
2020-11-01 20:10:35 -0300

    Music and Saw Rotations

---
+++ Assets/Scripts/MusicListener.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicListener : MonoBehaviour {
+   
+    // Start is called before the first frame update
+    void Start() {
+        DontDestroyOnLoad(gameObject);
+        if(FindObjectsOfType<MusicListener>().Length > 1) {
+            Destroy(gameObject);
+        }
+    }
+
+}
