c898d77222b22ebc015f7ccf80e6fc4929abd647
Pedrohph <micosacpv@hotmail.com>
2020-10-31 23:44:28 -0300

    Emergencial Update

---
+++ Assets/Scripts/Menu/TriggerRestartPos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerRestartPos : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetComponent<PlayerMovement>()) {
+            other.gameObject.transform.parent.position = new Vector3(0, 0, -30);
+        }
+    }
+}
