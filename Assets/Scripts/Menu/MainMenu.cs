cac9f001ab116f32dc1e1ccd4a6a08b43aa637eb
Raphael <raphaelalbino_@hotmail.com>
2020-11-01 20:07:57 -0300

    Main menu script updated

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -18,7 +18,7 @@ public class MainMenu : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetMouseButton(0) || Input.anyKeyDown) {
             Active();  
         }
     }
